ca57e3cd88197deedae505bfec7dd6ad5dde67da
kml777 <atakamal95@gmail.com>
2021-04-21 22:58:50 +0200

    Modify database_init

--- db_init.py
+++ db_init.py
@@ -10,7 +10,7 @@ def create_server_connection(host_name, user_name, user_password):
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
-            passwd=user_password
+            passwd=user_password,
         )
         print("MySQL Database connection successful")
     except Error as err:
@@ -29,8 +29,11 @@ def execute_query(connection, query):
         print(f"Error: '{err}'")
 
 
-def database_init():
+def database_init(create_database=False):
     create_database_query = "CREATE DATABASE scheduler"
+    check_database_exists_query = "SHOW DATABASES LIKE scheduler"
     connection = create_server_connection(host_name=configs.HOST_NAME, user_name=configs.USER_NAME,
                                           user_password=configs.PASSWORD)
-    execute_query(connection, create_database_query)
+    if not execute_query(connection, check_database_exists_query):
+        execute_query(connection, create_database_query)
+    return connection
