25f4b2cf877cf98319d80afe8b4180ca0597cd3c
kml777 <atakamal95@gmail.com>
2021-04-21 22:41:40 +0200

    Add execute_query

--- db_init.py
+++ db_init.py
@@ -17,3 +17,14 @@ def create_server_connection(host_name, user_name, user_password):
         print(f"Error: '{err}'")
 
     return connection
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
